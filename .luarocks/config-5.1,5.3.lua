b8bfc6ee7cc98117eb51cdad6b388dd84e5c3e3c
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2019-01-03 00:28:36 -0200

    I'm not familiar with LuaRocks, but I assume this folder is unnecessary.

--- .luarocks/config-5.1,5.3.lua
+++
@@ -1,7 +0,0 @@
--- LuaRocks configuration for use with Lua 5.1,5.3
-variables = {
-   LUA_DIR = "/usr/local",
-   LUA_INCDIR = "/usr/local/include",
-   LUA_LIBDIR = "/usr/local/lib",
-   LUA_BINDIR = "/usr/local/bin",
-}
