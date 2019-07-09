c6c5d5889d7564174b56e364902e8ce91ede5f2c
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-07-08 21:39:26 -0300

    titan version removed
    I probably will create a pallene in the future, but for now, just Lua is
    fine

--- motor_titan/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-  bit_ids  = require ('motor.bit_ids'),
-  entities = require ('motor.entities'),
-  states   = require ('motor.states'),
-  storages = require ('motor.storages'),
-  systems  = require ('motor.systems')
-}
