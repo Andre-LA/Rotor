d23988198014fd409893c526ef8e74f9388dc08e
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-10-12 17:15:01 -0300

    bitset_array spec was only working in Lua 5.3

--- spec/bitset_array_spec.lua
+++ spec/bitset_array_spec.lua
@@ -1,3 +1,60 @@
+---[[[ bitwise operators between Lua 5.1 (require LuaBitOp), 5.2 and 5.3
+local _lshift,
+      _rshift,
+      _bnot,
+      _band,
+      _bor,
+      _bxor;
+
+if _VERSION == "Lua 5.1" then
+  luabitop = require 'bit'
+  _lshift  = luabitop.lshift
+  _rshift  = luabitop.rshift
+  _bnot    = luabitop.bnot
+  _band    = luabitop.band
+  _bor     = luabitop.bor
+  _bxor    = luabitop.bxor
+elseif _VERSION == 'Lua 5.2' then
+  _lshift  = bit32.lshift
+  _rshift  = bit32.rshift
+  _bnot    = bit32.bnot
+  _band    = bit32.band
+  _bor     = bit32.bor
+  _bxor    = bit32.bxor
+else
+  _lshift = load('return function(x, y) return x << y end')()
+  _rshift = load('return function(x, y) return x >> y end')()
+  _bnot   = load('return function(x)    return ~x     end')()
+  _band   = load('return function(x, y) return x & y  end')()
+  _bor    = load('return function(x, y) return x | y  end')()
+  _bxor   = load('return function(x, y) return x ~ y  end')()
+end
+
+local function bitop_chain(v)
+  return {
+    value = v,
+    lshift = function(self, v) self.value = _lshift(self.value, v); return self end,
+    rshift = function(self, v) self.value = _rshift(self.value, v); return self end,
+    bnot   = function(self, v) self.value =   _bnot(self.value, v); return self end,
+    band   = function(self, v) self.value =   _band(self.value, v); return self end,
+    bor    = function(self, v) self.value =    _bor(self.value, v); return self end,
+    bxor   = function(self, v) self.value =   _bxor(self.value, v); return self end,
+    result = function(self) return self.value end
+  }
+end
+---]]]
+
+
+local bin = function(s)
+  return tonumber(s, 2)
+end
+
+local LEFTMOSTBIT = bitop_chain(0) -- 0000 [..] 0000
+                      :bnot()      -- 1111 [..] 1111
+                      :rshift(1)   -- 0111 [..] 1111
+                      :bnot()      -- 1000 [..] 0000
+                      :result()
+
 describe("#bitset_array library", function()
   local bitset_array = require 'motor.bitset_array'
 
@@ -87,15 +144,6 @@ describe("#bitset_array library", function()
     another_bitset[1] = 0x11  -- 0000 0001 0001
     another_bitset[2] = 0xb78 -- 1011 0111 1000
 
-    local bin = function(s)
-      return tonumber(s, 2)
-    end
-
-    local LEFTMOSTBIT = 1
-    while LEFTMOSTBIT > 0 do
-      LEFTMOSTBIT = LEFTMOSTBIT << 1
-    end
-
     local function run_func_with_bitset_array(op_name, other_value)
       local bitset_before = bitset_array.copy(bitset)
       local another_bitset_before = bitset_array.copy(other_value)
@@ -166,25 +214,25 @@ describe("#bitset_array library", function()
     end
 
     describe("'band' function", function ()
-      expected_result[1] = bitset[1] & another_bitset[1]
-      expected_result[2] = bitset[2] & another_bitset[2]
-      expected_result[3] = bitset[3] & 1
+      expected_result[1] = _band(bitset[1], another_bitset[1])
+      expected_result[2] = _band(bitset[2], another_bitset[2])
+      expected_result[3] = _band(bitset[3], 1)
 
       test_func_and_method('band', another_bitset, true)
     end)
 
     describe("'bor' function", function ()
-      expected_result[1] = bitset[1] | another_bitset[1]
-      expected_result[2] = bitset[2] | another_bitset[2]
-      expected_result[3] = bitset[3] | 0
+      expected_result[1] = _bor(bitset[1], another_bitset[1])
+      expected_result[2] = _bor(bitset[2], another_bitset[2])
+      expected_result[3] = _bor(bitset[3], 0)
 
       test_func_and_method('bor', another_bitset, true)
     end)
 
     describe("'bxor' function", function ()
-      expected_result[1] = bitset[1] ~ another_bitset[1]
-      expected_result[2] = bitset[2] ~ another_bitset[2]
-      expected_result[3] = bitset[3] ~ 0
+      expected_result[1] = _bxor(bitset[1], another_bitset[1])
+      expected_result[2] = _bxor(bitset[2], another_bitset[2])
+      expected_result[3] = _bxor(bitset[3], 0)
 
       test_func_and_method('bxor', another_bitset, true)
     end)
@@ -192,18 +240,21 @@ describe("#bitset_array library", function()
     describe("'lshift' function", function ()
       -- bitset[1]:
       -- 1111 0000 [..] 0000 0000 0000 0001
-      bitset[1] = 0x1
-        | LEFTMOSTBIT
-        | LEFTMOSTBIT >> 1
-        | LEFTMOSTBIT >> 2
-        | LEFTMOSTBIT >> 3
+
+      bitset[1] = bitop_chain(0x1)
+                    :bor(LEFTMOSTBIT)
+                    :bor(_rshift(LEFTMOSTBIT, 1))
+                    :bor(_rshift(LEFTMOSTBIT, 2))
+                    :bor(_rshift(LEFTMOSTBIT, 3))
+                    :result()
 
       -- bitset[2]:
       -- 1110 0000 [..] 0000 0000 0011 0010
-      bitset[2] = 0x32
-        | LEFTMOSTBIT
-        | LEFTMOSTBIT >> 1
-        | LEFTMOSTBIT >> 2
+      bitset[2] = bitop_chain(0x32)
+                    :bor(LEFTMOSTBIT)
+                    :bor(_rshift(LEFTMOSTBIT, 1))
+                    :bor(_rshift(LEFTMOSTBIT, 2))
+                    :result()
 
       -- bitset[3]:
       -- 0000 0000 0000 0000 1010 0100 0011
@@ -214,9 +265,17 @@ describe("#bitset_array library", function()
         -- 1100 0000 [..] 0000 0000 0110 0101
         -- 0000 0000 [..] 0001 0100 1000 0111
         describe("1 step", function()
-          expected_result[1] =
-            0x2 | LEFTMOSTBIT | LEFTMOSTBIT >> 1 | LEFTMOSTBIT >> 2
-          expected_result[2] = bin"1100101" | LEFTMOSTBIT | LEFTMOSTBIT >> 1
+          expected_result[1] = bitop_chain(0x2)
+                               :bor(LEFTMOSTBIT)
+                               :bor(_rshift(LEFTMOSTBIT, 1))
+                               :bor(_rshift(LEFTMOSTBIT, 2))
+                               :result()
+
+          expected_result[2] = bitop_chain(bin"1100101")
+                                :bor(LEFTMOSTBIT)
+                                :bor(_rshift(LEFTMOSTBIT, 1))
+                                :result()
+
           expected_result[3] = bin"1010010000111"
 
 
@@ -228,8 +287,11 @@ describe("#bitset_array library", function()
         -- 1000 0000 [..] 0000 0000 1100 1011
         -- 0000 0000 [..] 0010 1001 0000 1111
         describe("2 steps", function()
-          expected_result[1] = bin"100" | LEFTMOSTBIT | LEFTMOSTBIT >> 1
-          expected_result[2] = bin"11001011" | LEFTMOSTBIT
+          expected_result[1] = bitop_chain(bin"100")
+                                :bor(LEFTMOSTBIT)
+                                :bor(_rshift(LEFTMOSTBIT, 1))
+                                :result()
+          expected_result[2] = _bor(bin"11001011", LEFTMOSTBIT)
           expected_result[3] = bin"10100100001111"
 
           test_func_and_method('lshift', 2)
@@ -240,7 +302,7 @@ describe("#bitset_array library", function()
         -- 0000 0000 [..] 0000 0001 1001 0111
         -- 0000 0000 [..] 0101 0010 0001 1111
         describe("3 steps", function()
-          expected_result[1] = bin"1000" | LEFTMOSTBIT
+          expected_result[1] = _bor(bin"1000", LEFTMOSTBIT)
           expected_result[2] = bin"110010111"
           expected_result[3] = bin"101001000011111"
 
@@ -264,11 +326,11 @@ describe("#bitset_array library", function()
     describe("'rshift' function", function ()
       -- bitset[1]:
       -- 1000 0000 [..] 0000 0000 0000 0001
-      bitset[1] = 0x1 | LEFTMOSTBIT
+      bitset[1] = _bor(0x1, LEFTMOSTBIT)
 
       -- bitset[2]:
       -- 1000 0000 [..] 0000 0000 0011 0010
-      bitset[2] = 0x32 | LEFTMOSTBIT
+      bitset[2] = _bor(0x32, LEFTMOSTBIT)
 
       -- bitset[3]:
       -- 0000 0000 0000 0000 1010 0100 0011
@@ -279,8 +341,11 @@ describe("#bitset_array library", function()
         -- 1100 0000 [..] 0000 0000 0001 1001
         -- 0000 0000 0000 0000 0101 0010 0001
         describe("1 step", function()
-          expected_result[1] = 0 | LEFTMOSTBIT >> 1
-          expected_result[2] = bin"11001" | LEFTMOSTBIT | LEFTMOSTBIT >> 1
+          expected_result[1] = _bor(0, _rshift(LEFTMOSTBIT, 1))
+          expected_result[2] = bitop_chain(bin"11001")
+                                :bor(LEFTMOSTBIT)
+                                :bor(_rshift(LEFTMOSTBIT, 1))
+                                :result()
           expected_result[3] = bin"10100100001"
 
           test_func_and_method('rshift', 1)
@@ -291,9 +356,15 @@ describe("#bitset_array library", function()
         -- 1110 0000 [..] 0000 0000 0000 1100
         -- 0000 0000 0000 0000 0010 1001 0000
         describe("2 steps", function()
-          expected_result[1] = 0 | LEFTMOSTBIT | LEFTMOSTBIT >> 2
-          expected_result[2] =
-            bin"1100" | LEFTMOSTBIT | LEFTMOSTBIT >> 1 | LEFTMOSTBIT >> 2
+          expected_result[1] = bitop_chain(0)
+                                :bor(LEFTMOSTBIT)
+                                :bor(_rshift(LEFTMOSTBIT, 2))
+                                :result()
+          expected_result[2] = bitop_chain(bin"1100")
+                                :bor(LEFTMOSTBIT)
+                                :bor(_rshift(LEFTMOSTBIT, 1))
+                                :bor(_rshift(LEFTMOSTBIT, 2))
+                                :result()
           expected_result[3] = bin"1010010000"
 
           test_func_and_method('rshift', 2)
@@ -304,9 +375,15 @@ describe("#bitset_array library", function()
         -- 0111 0000 [..] 0000 0000 0000 0110
         -- 0000 0000 0000 0000 0001 0100 1000
         describe("3 steps", function()
-          expected_result[1] = 0 | LEFTMOSTBIT >> 1 | LEFTMOSTBIT >> 3
-          expected_result[2] =
-            bin"110" | LEFTMOSTBIT >> 1 | LEFTMOSTBIT >> 2 | LEFTMOSTBIT >> 3
+          expected_result[1] = bitop_chain(0)
+                                :bor(_rshift(LEFTMOSTBIT, 1))
+                                :bor(_rshift(LEFTMOSTBIT, 3))
+                                :result()
+          expected_result[2] = bitop_chain(bin"110")
+                                :bor(_rshift(LEFTMOSTBIT, 1))
+                                :bor(_rshift(LEFTMOSTBIT, 2))
+                                :bor(_rshift(LEFTMOSTBIT, 3))
+                                :result()
           expected_result[3] = bin"101001000"
 
           test_func_and_method('rshift', 3)
@@ -317,9 +394,15 @@ describe("#bitset_array library", function()
         -- 0011 1000 [..] 0000 0000 0000 0011
         -- 0000 0000 0000 0000 0000 1010 0100
         describe("4 steps", function()
-          expected_result[1] = 0 | LEFTMOSTBIT >> 2 | LEFTMOSTBIT >> 4
-          expected_result[2] =
-            bin"11" | LEFTMOSTBIT >> 2 | LEFTMOSTBIT >> 3| LEFTMOSTBIT >> 4
+          expected_result[1] = bitop_chain(0)
+                                :bor(_rshift(LEFTMOSTBIT, 2))
+                                :bor(_rshift(LEFTMOSTBIT, 4))
+                                :result()
+          expected_result[2] = bitop_chain(bin"11")
+                                :bor(_rshift(LEFTMOSTBIT, 2))
+                                :bor(_rshift(LEFTMOSTBIT, 3))
+                                :bor(_rshift(LEFTMOSTBIT, 4))
+                                :result()
           expected_result[3] = bin"10100100"
 
           test_func_and_method('rshift', 4)
@@ -328,9 +411,9 @@ describe("#bitset_array library", function()
     end)
 
     describe("'bnot' function", function ()
-      expected_result[1] = ~bitset[1]
-      expected_result[2] = ~bitset[2]
-      expected_result[3] = ~bitset[3]
+      expected_result[1] = _bnot(bitset[1])
+      expected_result[2] = _bnot(bitset[2])
+      expected_result[3] = _bnot(bitset[3])
 
       test_func_and_method('bnot')
     end)
