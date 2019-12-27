8f380b002f87caf1da7a7d481b1c2e3d6c5dc2f1
André L. A <andrluiz.alvares.alvares84@gmail.com>
2019-12-27 20:43:56 -0300

    lua5x-operations new function: union
    ```Lua
    union({1, 2, 3}, {4, 5, 6}) --> {1, 2, 3, 4, 5, 6}
    ```

--- motor/lua5x-operations/5_2.lua
+++ motor/lua5x-operations/5_2.lua
@@ -7,8 +7,14 @@ return {
    band   = bit32.band,
    bor    = bit32.bor,
    bxor   = bit32.bxor,
-   rshift = bit32.rshift,
    lshift = bit32.lshift,
+   rshift = bit32.rshift,
    bnot   = bit32.bnot,
-   unpack = table.unpack
+   unpack = table.unpack,
+   union  =  function (a, b)
+      local t, a_len = {}, #a
+      for i = 1, #a do t[i] = a[i] end
+      for i = 1, #b do t[i+a_len] = b[i] end
+      return t
+   end
 }
--- motor/lua5x-operations/5_3.lua
+++ motor/lua5x-operations/5_3.lua
@@ -10,5 +10,11 @@ return {
    lshift = function (l, r) return l << r end,
    rshift = function (l, r) return l >> r end,
    bnot   = function (v) return ~v end,
-   unpack = table.unpack
+   unpack = table.unpack,
+   union  =  function (a, b)
+      local t, a_len = {}, #a
+      for i = 1, #a do t[i] = a[i] end
+      for i = 1, #b do t[i+a_len] = b[i] end
+      return t
+   end
 }
